c6c32498f8590d8346097259ff5dcf65e852f4a9
& <eae>
2021-10-12 10:37:05 +0200

    Auto stash before merge of "feature/notification_service" and "origin/feature/notification_service"

--- services/alert/src/main/java/com/hospyboard/api/alert/mappers/AlertMapper.java
+++ services/alert/src/main/java/com/hospyboard/api/alert/mappers/AlertMapper.java
@@ -7,7 +7,11 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AlertMapper {
+<<<<<<< Updated upstream
     @Mapping(target = "id", ignore = true)
+=======
+
+>>>>>>> Stashed changes
     AlertEntity toEntity(AlertDTO dto);
 
     AlertDTO toDto(AlertEntity entity);
