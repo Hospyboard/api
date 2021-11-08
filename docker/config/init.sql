3744dc79806814f9474ad1708b41058f7c26d950
Antoine <funixgaming7@gmail.com>
2021-11-08 18:26:16 +0200

    added permissions to user on tables in init script

--- docker/config/init.sql
+++ docker/config/init.sql
@@ -7,3 +7,6 @@ CREATE DATABASE hospybord_hospital;
 CREATE USER hospyboard WITH PASSWORD 'admin';
 GRANT USAGE ON SCHEMA keycloak TO hospyboard;
 GRANT USAGE ON SCHEMA hospybord_user TO hospyboard;
+GRANT USAGE ON SCHEMA hospybord_notification TO hospyboard;
+GRANT USAGE ON SCHEMA hospybord_alert TO hospyboard;
+GRANT USAGE ON SCHEMA hospybord_hospital TO hospyboard;
