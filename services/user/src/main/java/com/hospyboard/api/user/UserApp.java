10d43df690414d3cb1e22fc7ce437e401cf1e9cc
& <eae>
2022-01-10 12:31:13 +0100

    swagger on user and hospital

--- services/user/src/main/java/com/hospyboard/api/user/UserApp.java
+++ services/user/src/main/java/com/hospyboard/api/user/UserApp.java
@@ -3,9 +3,11 @@ package com.hospyboard.api.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableFeignClients("com.hospyboard.api")
 @SpringBootApplication(scanBasePackages = "com.hospyboard.api")
+@EnableSwagger2
 public class UserApp {
     public static void main(String[] args) {
         SpringApplication.run(UserApp.class, args);
