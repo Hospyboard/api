e948258b0eff6fb21cc69dce14fa1f2658468f44
Antoine PRONNIER (Funix) <funixgaming7@gmail.com>
2022-01-26 15:07:02 +0100

    user register and login WORKING

--- src/main/java/com/hospyboard/api/HospyboardApiApp.java
+++ src/main/java/com/hospyboard/api/HospyboardApiApp.java
@@ -2,8 +2,6 @@ package com.hospyboard.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = "com.hospyboard.api")
@@ -11,10 +9,5 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class HospyboardApiApp {
     public static void main(String[] args) {
         SpringApplication.run(HospyboardApiApp.class, args);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/hospyboard/api/user/services/UserService.java
+++ src/main/java/com/hospyboard/api/user/services/UserService.java
@@ -3,17 +3,14 @@ package com.hospyboard.api.user.services;
 import com.hospyboard.api.user.dto.UserCreationDTO;
 import com.hospyboard.api.user.dto.UserDTO;
 import com.hospyboard.api.user.entity.User;
-import com.hospyboard.api.user.exception.LoginHospyboardException;
 import com.hospyboard.api.user.exception.RegisterHospyboardException;
 import com.hospyboard.api.user.mappers.UserMapper;
 import com.hospyboard.api.user.repository.UserRepository;
-import org.springframework.context.annotation.Bean;
-import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -24,16 +21,16 @@ public class UserService implements UserDetailsService {
     private final CurrentUser currentUser;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository,
                        UserMapper userMapper,
                        CurrentUser currentUser,
-                       BCryptPasswordEncoder bCryptPasswordEncoder) {
+                       PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.currentUser = currentUser;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Nullable
@@ -46,7 +43,7 @@ public class UserService implements UserDetailsService {
         if (userCreationDTO.getPassword().equals(userCreationDTO.getPasswordConfirmation())) {
             final User user = this.userMapper.fromUserCreationToEntity(userCreationDTO);
 
-            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             return this.userMapper.toDto(this.userRepository.save(user));
         } else {
             throw new RegisterHospyboardException("Vos mots de passe ne correspondent pas.");
