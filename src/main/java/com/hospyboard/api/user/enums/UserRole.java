a2566c71c268606f7fc17c00200ba4570a03916a
Antoine PRONNIER (Funix) <funixgaming7@gmail.com>
2022-02-03 17:31:39 +0200

    Unit tests done

--- src/main/java/com/hospyboard/api/user/enums/UserRole.java
+++ src/main/java/com/hospyboard/api/user/enums/UserRole.java
@@ -4,13 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
-@Getter
-@Setter
-public class UserRole implements GrantedAuthority {
-
+public class UserRole {
     public static final String PATIENT = "PATIENT";
     public static final String HOSPYTAL_WORKER = "HOSPYTAL_WORKER";
     public static final String ADMIN = "ADMIN";
-
-    private String authority;
 }
