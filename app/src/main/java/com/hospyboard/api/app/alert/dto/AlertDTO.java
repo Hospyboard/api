e7249ee91d8ee6ae718e2e2f2f83ef15c1b2b000
Antoine PRONNIER (Funix) <funixgaming7@gmail.com>
2022-04-29 18:21:02 +0300

    clean code

--- app/src/main/java/com/hospyboard/api/app/alert/dto/AlertDTO.java
+++ app/src/main/java/com/hospyboard/api/app/alert/dto/AlertDTO.java
@@ -7,12 +7,22 @@ import fr.funixgaming.api.core.crud.dtos.ApiDTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class AlertDTO extends ApiDTO {
+    @NotBlank
     private String patientUuid;
+
     private String staffUuid;
+
+    @NotBlank
     private AlertType type;
+
+    @NotBlank
     private AlertImportance importance;
+
+    @NotBlank
     private AlertStatus status;
 }
