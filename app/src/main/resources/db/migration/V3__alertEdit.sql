06a1e11e14f50728d83138fb21b077473d326247
Antoine PRONNIER (Funix) <funixgaming7@gmail.com>
2022-05-05 18:54:49 +0300

    docker file build and changes on alerts

--- app/src/main/resources/db/migration/V3__alertEdit.sql
+++ app/src/main/resources/db/migration/V3__alertEdit.sql
@@ -1,5 +1,7 @@
 alter table alert_entity drop column patient_uuid;
 alter table alert_entity drop column staff_uuid;
 
-alter table alert_entity add column patient_id bigint not null;
-alter table alert_entity add column staff_id   bigint null;
+alter table alert_entity add column patient_id bigint         not null;
+alter table alert_entity add column staff_id   bigint         null;
+alter table alert_entity add column infos      varchar(10000) null;
+
