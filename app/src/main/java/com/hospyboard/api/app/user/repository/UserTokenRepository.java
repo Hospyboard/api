df94741675c048eb94a6d89077aaa1a33fc53782
Antoine PRONNIER (Funix) <funixgaming7@gmail.com>
2022-05-06 20:50:20 +0300

    tests ok

--- app/src/main/java/com/hospyboard/api/app/user/repository/UserTokenRepository.java
+++ app/src/main/java/com/hospyboard/api/app/user/repository/UserTokenRepository.java
@@ -1,9 +1,13 @@
 package com.hospyboard.api.app.user.repository;
 
+import com.hospyboard.api.app.user.entity.User;
 import com.hospyboard.api.app.user.entity.UserToken;
 import fr.funixgaming.api.core.crud.repositories.ApiRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface UserTokenRepository extends ApiRepository<UserToken> {
+    Set<UserToken> findAllByUser(User user);
 }
