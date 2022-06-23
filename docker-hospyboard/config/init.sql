f4ba8ec46be2fec68e71007031b2b1c6c8b95df3
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-06-23 17:23:56 +0200

    migration script

--- docker-hospyboard/config/init.sql
+++ docker-hospyboard/config/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS hospyboard;
+CREATE DATABASE IF NOT EXISTS hospyboard_docker;
 
 CREATE USER 'admin'@'%' IDENTIFIED BY 'adminpass';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
