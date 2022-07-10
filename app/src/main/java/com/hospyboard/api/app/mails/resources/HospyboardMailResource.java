551a083c027f3b38e5e08a43f9278b1c3b3d401c
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-07-10 17:26:34 +0200

    Add route to contact staff

--- app/src/main/java/com/hospyboard/api/app/mails/resources/HospyboardMailResource.java
+++ app/src/main/java/com/hospyboard/api/app/mails/resources/HospyboardMailResource.java
@@ -2,7 +2,9 @@ package com.hospyboard.api.app.mails.resources;
 
 import com.hospyboard.api.app.mails.dtos.HospyboardMailDTO;
 import com.hospyboard.api.app.mails.services.HospyboardMailService;
+import fr.funixgaming.api.core.exceptions.ApiBadRequestException;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,20 @@ public class HospyboardMailResource {
 
     @PostMapping
     public void sendMail(@RequestBody HospyboardMailDTO request) {
+        mailService.getMailQueue().add(request);
+    }
+
+    @PostMapping("contact")
+    public void sendContactMail(@RequestBody HospyboardMailDTO request) {
+        request.setTo("hospyboard_2023@labeip.epitech.eu");
+
+        if (Strings.isEmpty(request.getFrom())) {
+            throw new ApiBadRequestException("No from email");
+        } else if (Strings.isEmpty(request.getSubject())) {
+            throw new ApiBadRequestException("No subject");
+        } else if (Strings.isEmpty(request.getText())) {
+            throw new ApiBadRequestException("No text");
+        }
         mailService.getMailQueue().add(request);
     }
 
