cd91a64e8b08a540c2ab1ae5c264dd89acc1b911
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-07-10 16:59:38 +0200

    Fix merge

--- app/src/main/java/com/hospyboard/api/app/user/repository/UserRepository.java
+++ app/src/main/java/com/hospyboard/api/app/user/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.hospyboard.api.app.user.repository;
 
 import com.hospyboard.api.app.user.entity.User;
 import fr.funixgaming.api.core.crud.repositories.ApiRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,7 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends ApiRepository<User> {
     Optional<User> findByUsername(String username);
-    Iterable<User> findAllByRoomUuid(String roomUuid);
     Iterable<User> findAllByEmail(String email);
     Iterable<User> findAllByRoomUuid(String roomUuid);
 }
