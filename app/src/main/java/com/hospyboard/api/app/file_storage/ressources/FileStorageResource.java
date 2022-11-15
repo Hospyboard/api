fa71e59ef9358a13db283747aba6d450d5bf4197
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-11-16 00:26:06 +0100

    Upload done

---
+++ app/src/main/java/com/hospyboard/api/app/file_storage/ressources/FileStorageResource.java
@@ -0,0 +1,83 @@
+package com.hospyboard.api.app.file_storage.ressources;
+
+import com.hospyboard.api.app.file_storage.dtos.FileDTO;
+import com.hospyboard.api.app.file_storage.services.FileStorageService;
+import com.hospyboard.api.app.user.services.CurrentUser;
+import fr.funixgaming.api.core.crud.dtos.PageDTO;
+import fr.funixgaming.api.core.exceptions.ApiBadRequestException;
+import fr.funixgaming.api.core.exceptions.ApiException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("files")
+public class FileStorageResource {
+
+    private final FileStorageService fileStorageService;
+    private final CurrentUser currentUser;
+    private final Set<String> fileExtensionsValid = Set.of(
+            ".txt",
+            ".pdf",
+            ".png",
+            ".jpeg",
+            ".gif",
+            ".mp4",
+            ".mp3"
+    );
+    private final String fileExtensionsValidString = String.join(" ", fileExtensionsValid);
+
+    public FileStorageResource(FileStorageService fileStorageService,
+                               CurrentUser currentUser) {
+        this.fileStorageService = fileStorageService;
+        this.currentUser = currentUser;
+    }
+
+    @GetMapping
+    public PageDTO<FileDTO> getFiles(@RequestParam(value = "page", defaultValue = "0") String page,
+                                     @RequestParam(value = "elemsPerPage", defaultValue = "300") String elemsPerPage,
+                                     @RequestParam(value = "search", defaultValue = "") String search,
+                                     @RequestParam(value = "sort", defaultValue = "") String sort) {
+        return this.fileStorageService.getAll(page, elemsPerPage, search, sort);
+    }
+
+    @PostMapping
+    public FileDTO uploadFile(@RequestParam @NotNull final MultipartFile file) {
+        try {
+            if (isFileValid(file)) {
+                final FileDTO fileDTO = new FileDTO();
+
+                fileDTO.setFile(file.getBytes());
+                fileDTO.setFileOwner(currentUser.getCurrentUser());
+                fileDTO.setFileName(String.format("%s-%s", UUID.randomUUID(), file.getResource().getFilename()));
+                fileDTO.setFileSize(file.getSize());
+                return fileStorageService.create(fileDTO);
+            } else {
+                throw new ApiBadRequestException("Format de fichier invalide. Voici les formats valides : " + fileExtensionsValidString);
+            }
+        } catch (IOException e) {
+            throw new ApiException("Impossible de décoder le fichier reçu.", e);
+        }
+    }
+
+    @DeleteMapping
+    public void deleteFile(@RequestParam String id) {
+        this.fileStorageService.delete(id);
+    }
+
+    private boolean isFileValid(final MultipartFile file) {
+        for (final String ext : fileExtensionsValid) {
+            final String fName = file.getResource().getFilename();
+
+            if (fName != null && fName.endsWith(ext)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ app/src/main/java/com/hospyboard/api/app/file_storage/services/FileStorageService.java
@@ -0,0 +1,97 @@
+package com.hospyboard.api.app.file_storage.services;
+
+import com.hospyboard.api.app.file_storage.dtos.FileDTO;
+import com.hospyboard.api.app.file_storage.entities.FileEntity;
+import com.hospyboard.api.app.file_storage.mappers.FilesMapper;
+import com.hospyboard.api.app.file_storage.repositories.FilesRepository;
+import com.hospyboard.api.app.user.entity.User;
+import com.hospyboard.api.app.user.repository.UserRepository;
+import fr.funixgaming.api.core.crud.services.ApiService;
+import fr.funixgaming.api.core.exceptions.ApiBadRequestException;
+import fr.funixgaming.api.core.exceptions.ApiException;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Optional;
+import java.util.Set;
+
+@Slf4j
+@Service
+public class FileStorageService extends ApiService<FileDTO, FileEntity, FilesMapper, FilesRepository> {
+
+    private final File filesFolder = new File("files_storage");
+    private final UserRepository userRepository;
+
+    public FileStorageService(FilesRepository repository,
+                              UserRepository userRepository,
+                              FilesMapper mapper) throws ApiException {
+        super(repository, mapper);
+        this.userRepository = userRepository;
+
+        try {
+            if (!filesFolder.exists() && !filesFolder.mkdir()) {
+                throw new IOException("files_storage can't be created.");
+            }
+        } catch (IOException e) {
+            throw new ApiException("Impossible de créer le dossier qui contient les fichiers upload.", e);
+        }
+    }
+
+    @Override
+    public void beforeSavingEntity(@NonNull FileDTO request, @NonNull FileEntity entity) {
+        if (request.getId() == null) {
+            final File userFolder = new File(filesFolder, request.getFileOwner().getId().toString());
+
+            try {
+                if (!userFolder.exists() && !userFolder.mkdir()) {
+                    throw new IOException("Could not create user folder.");
+                }
+                final File fileToSave = new File(userFolder, request.getFileName());
+                if (!fileToSave.exists() && !fileToSave.createNewFile()) {
+                    throw new IOException("Could not create final file.");
+                }
+                Files.write(fileToSave.toPath(), request.getFile());
+
+                final User fileOwner = findUserByUuid(request.getFileOwner().getId().toString());
+                entity.setFileOwner(fileOwner);
+                entity.setFilePath(fileToSave.getPath());
+            } catch (IOException e) {
+                throw new ApiException(String.format("Impossible d'enregistrer le fichier. Erreur: %s", e.getMessage()), e);
+            }
+        } else {
+            throw new ApiBadRequestException("Vous ne pouvez pas modifier les data des fichiers.");
+        }
+    }
+
+    @Override
+    public void beforeDeletingEntity(@NonNull FileEntity entity) {
+        final File file = new File(entity.getFilePath());
+
+        if (!file.delete()) {
+            log.info("Le fichier {} n'existe pas, pas besoin de le supprimer.", entity.getFilePath());
+        }
+    }
+
+    public void deleteAllFilesByUser(final User user) {
+        final Set<FileEntity> files = super.getRepository().findAllByFileOwner(user);
+
+        for (final FileEntity file : files) {
+            beforeDeletingEntity(file);
+        }
+        super.getRepository().deleteAll(files);
+    }
+
+    private User findUserByUuid(final String userUuid) {
+        final Optional<User> search = userRepository.findByUuid(userUuid);
+
+        if (search.isPresent()) {
+            return search.get();
+        } else {
+            throw new ApiBadRequestException("L'utilisateur demandé n'existe pas.");
+        }
+    }
+}
